aeeb4883f5eb60af459f92012c9023a3490ade30
Oratile Ditlhage <96663869+Exodera@users.noreply.github.com>
2022-10-13 20:09:59 +0200

    initial commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using FirstMvcApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMvcApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            DogViewModel doggo = new DogViewModel
+            {
+                Name = "Stevie",
+                Age = 3
+            };
+
+            return View(doggo);
+        }
+
+        public string Hello()
+        {
+            return "Who's there?";
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
